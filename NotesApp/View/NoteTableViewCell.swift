b47c361cd6422c40d84be3e457fa12237122274e
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-20 21:22:54 +0300

    added description label

--- NotesApp/View/NoteTableViewCell.swift
+++ NotesApp/View/NoteTableViewCell.swift
@@ -4,6 +4,7 @@ final class NoteTableViewCell: UITableViewCell {
     // MARK: - GUI
     private let containerView = UIView()
     private let titleLabel = UILabel()
+    private let descriptionLabel = UILabel()
     
     // MARK: - Init's
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -11,6 +12,7 @@ final class NoteTableViewCell: UITableViewCell {
         
         configureContainerView()
         configureTitleLabel()
+        configureDescriptionLabel()
     }
     required init?(coder: NSCoder) {
         fatalError()
@@ -18,6 +20,7 @@ final class NoteTableViewCell: UITableViewCell {
     // MARK: - Methods
     func setupCell(with cell: NoteViewModel) {
         self.titleLabel.text = cell.title
+        self.descriptionLabel.text = cell.description
     }
 }
 
@@ -34,16 +37,18 @@ private extension NoteTableViewCell {
         
         containerView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
-            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5),
-            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
-            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5)
+            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 4),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -4)
         ])
     }
     
     // MARK: - configure titleLabel
     func configureTitleLabel() {
+        titleLabel.textAlignment = .left
         titleLabel.font = .boldSystemFont(ofSize: 18)
+        titleLabel.numberOfLines = 1
         
         configureTitleLabelLayout()
     }
@@ -52,10 +57,30 @@ private extension NoteTableViewCell {
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor, constant: 10),
-            titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -10),
-            titleLabel.bottomAnchor.constraint(equalTo: self.containerView.bottomAnchor, constant: -10)
+            titleLabel.topAnchor.constraint(equalTo: self.containerView.topAnchor, constant: 4),
+            titleLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 4),
+            titleLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -4),
+            titleLabel.heightAnchor.constraint(equalToConstant: 24)
+        ])
+    }
+    
+    // MARK: - configure descriptionLabel
+    func configureDescriptionLabel() {
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.font = .systemFont(ofSize: 12)
+        descriptionLabel.textColor = UIColor.black
+        
+        configureDescriptionLabelLayout()
+    }
+    func configureDescriptionLabelLayout() {
+        self.containerView.addSubview(descriptionLabel)
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor, constant: 4),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -4),
+            descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: self.containerView.bottomAnchor)
         ])
     }
 }
