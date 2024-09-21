9618e313698403119ff96239aa9728721f211869
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-21 22:56:25 +0300

    added methods for barbuttons

--- NotesApp/Detail/ViewModel/DetailViewModel.swift
+++ NotesApp/Detail/ViewModel/DetailViewModel.swift
@@ -1,10 +1,16 @@
+import Foundation
+
 protocol DetailViewModelProtocol {
     func getTitle() -> String?
     func getDescription() -> String?
+    func createNote(title: String?, description: String?, urlToImage: String?, date: Date)
+    func deleteNote()
+    func getTitleAndDescription(from text: String) -> (String?, String?)
 }
 final class DetailViewModel: DetailViewModelProtocol {
     // MARK: - Properties
     private let note: NoteViewModel?
+    private let coreDataManager = CoreDataManager.shared
     
     // MARK: - Init
     init(note: NoteViewModel?) {
@@ -16,5 +22,31 @@ final class DetailViewModel: DetailViewModelProtocol {
     }
     func getDescription() -> String? {
         return self.note?.description
+    }
+    // MARK: - Methods
+    func createNote(title: String?, description: String?, urlToImage: String?, date: Date) {
+        self.coreDataManager.createNote(title: title, description: description, urlToImage: urlToImage, date: date)
+    }
+    func deleteNote() {
+        guard let note = self.note else { return }
+        self.coreDataManager.deleteNote(note: note)
+    }
+    func getTitleAndDescription(from text: String) -> (String?, String?) {
+        let tulp: (String?, String?)
+        
+        let firstIndex = text.startIndex
+        let lastIndex = text.firstIndex(where: {["?","!",".","\n"].contains($0)}) ?? text.endIndex
+        let title = String(text[firstIndex..<lastIndex]).trimmingCharacters(in: .whitespacesAndNewlines)
+        if lastIndex == text.endIndex || text.index(after: lastIndex) == text.endIndex {
+            tulp.1 = nil
+        } else {
+            let description = String(text[text.index(after: lastIndex)..<text.endIndex]).trimmingCharacters(in: .whitespacesAndNewlines)
+            tulp.1 = description.isEmpty ? nil : description
+        }
+        tulp.0 = title.isEmpty ? nil : title
+        
+        
+        
+        return tulp
     }
 }
