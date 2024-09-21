0903f3832f34a1829f48f39a595b55c457a7cb99
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-21 22:58:03 +0300

    added reloadData closure, notificationcenter observer

--- NotesApp/Table/ViewModel/TableViewModel.swift
+++ NotesApp/Table/ViewModel/TableViewModel.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+
 protocol TableViewModelProtocol {
     func getNumberOfNotes(section: Int) -> Int
     func getNumberOfSections() -> Int
@@ -9,13 +10,19 @@ protocol TableViewModelProtocol {
 final class TableViewModel: TableViewModelProtocol {
     var reloadTable: (() -> Void)?
     // MARK: - Properties
-    private var sections: [SectionViewModel] = []
+    private var sections: [SectionViewModel] = [] {
+        didSet {
+            self.reloadTable?()
+        }
+    }
     private let coreDataManager = CoreDataManager.shared
     
     // MARK: - Init
     init() {
         #warning("mock")
-        self.setMockNotes()
+        //self.setMockNotes()
+        self.registerObserver()
+        self.loadNotes()
     }
     
     // MARK: - Methods
@@ -28,6 +35,13 @@ final class TableViewModel: TableViewModelProtocol {
     func getNote(indexPath: IndexPath) -> NoteViewModel {
         return self.sections[indexPath.section].notes[indexPath.item]
     }
+    @objc private func loadNotes() {
+        let firstSection = SectionViewModel()
+        let notes = self.coreDataManager.fetchNotes().map {NoteViewModel(noteCoreDataModel: $0)}
+        firstSection.notes = notes
+        self.sections = [firstSection]
+    }
+
     private func setMockNotes() {
 //        let firstSection = SectionViewModel()
 //        let firstMock = NoteModel(title: "Записаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачу", description: "На вечер. К терапевту", urlToImage: nil, date: Date())
@@ -36,11 +50,14 @@ final class TableViewModel: TableViewModelProtocol {
 //        self.sections = [firstSection]
         
         let firstSection = SectionViewModel()
-        self.coreDataManager.createNote(title: "Записаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачу", description: "На вечер. К терапевту", urlToImage: nil, date: Date())
-        self.coreDataManager.createNote(title: "Пароль к телефону", description: "12345", urlToImage: nil, date: Date())
+//        self.coreDataManager.createNote(title: "Записаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачуЗаписаться к врачу", description: "На вечер. К терапевту", urlToImage: nil, date: Date())
+//        self.coreDataManager.createNote(title: "Пароль к телефону", description: "12345", urlToImage: nil, date: Date())
         let notes = self.coreDataManager.fetchNotes().map {NoteViewModel(noteCoreDataModel: $0)}
         firstSection.notes = notes
         self.sections = [firstSection]
         
+    }
+    private func registerObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(loadNotes), name: NSNotification.Name("FetchNotes"), object: nil)
     }
 }
