304b06e48d6266f6d27ad80e01cb3a8682cc7b47
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-25 22:33:36 +0300

    refactored FileManagerPersistent to get urlToImage

--- NotesApp/FileManagerPersistent.swift
+++ NotesApp/FileManagerPersistent.swift
@@ -7,7 +7,8 @@ final class FileManagerPersistent {
     private init() {}
     // MARK: - Methods
     func save(_ data: Data?, name: String?) -> URL? {
-        guard let name = name, let url = self.getDocumentDirectory()?.appending(path: name), let data = data else { return nil}
+        guard let name = name, let data = data else { return nil}
+        let url = self.getDocumentDirectory().appending(path: name)
         do {
             try data.write(to: url)
             print("Image was saved = \(url)")
@@ -18,7 +19,22 @@ final class FileManagerPersistent {
             return nil
         }
     }
-    func getDocumentDirectory() -> URL? {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    func read(from url: URL) -> Data? {
+        if FileManager.default.fileExists(atPath: url.path) {
+            do {
+                let data = try Data(contentsOf: url)
+                return data
+            } catch {
+                let nserror = error as NSError
+                print("Unsolved error \(nserror), \(nserror.userInfo)")
+                return nil
+            }
+        } else {
+            print("Файл не существует по пути: \(url.path)")
+            return nil
+        }
+    }
+    func getDocumentDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
     }
 }
