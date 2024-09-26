8e0188398457c70ce867a2a982d22e65c3c49ff0
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-26 22:37:11 +0300

    changed note color

--- NotesApp/Extensions/UIColor+Ex.swift
+++ NotesApp/Extensions/UIColor+Ex.swift
@@ -1,5 +1,5 @@
 import UIKit
 
 extension UIColor {
-    static let lightYellow = UIColor(red: 255/255, green: 241/255, blue: 137/255, alpha: 1.0)
+    static let lightYellow = UIColor(hue: 58/360, saturation: 26/100, brightness: 99/100, alpha: 1.0)
 }
