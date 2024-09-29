6ac529849bc1b988919db244c21f1b7ebe4a31e4
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-09-29 23:38:44 +0300

    added a static array in UIColor to create a button's menu

--- NotesApp/Extensions/UIColor+Ex.swift
+++ NotesApp/Extensions/UIColor+Ex.swift
@@ -3,4 +3,5 @@ import UIKit
 extension UIColor {
     static let lightYellow = UIColor(hue: 58/360, saturation: 26/100, brightness: 99/100, alpha: 1.0)
     static let lightBlue = UIColor(red: 149/255, green: 227/255, blue: 244/255, alpha: 1.0)
+    static let additionalColors: [UIColor] = [.lightYellow, .lightBlue]
 }
