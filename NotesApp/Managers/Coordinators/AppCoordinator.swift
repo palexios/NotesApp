cf44130ff67739e0965f0f277abdd8f717ef577c
- <->
2024-10-03 21:06:48 +0300

    implemented AppCoordinator class

--- NotesApp/Managers/Coordinators/AppCoordinator.swift
+++ NotesApp/Managers/Coordinators/AppCoordinator.swift
@@ -1,8 +1,29 @@
-//
-//  AppCoordinator.swift
-//  NotesApp
-//
-//  Created by Александр Павлицкий on 03.10.2024.
-//
+import UIKit
 
-import Foundation
+final class AppCoordinator: Coordinator {
+    // MARK: - Properties
+    var navigationController: UINavigationController
+    
+    // MARK: - Init
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - Methods
+    func start() {
+        showMain()
+    }
+    
+    func showMain() {
+        let tableViewModel = TableViewModel()
+        let tableViewController = TableViewController(viewModel: tableViewModel)
+        tableViewController.coordinator = self
+        self.navigationController.pushViewController(tableViewController, animated: true)
+    }
+    
+    func showDetail(with noteViewModel: NoteViewModel) {
+        let detailViewModel = DetailViewModel(note: noteViewModel)
+        let detailViewController = DetailViewController(viewModel: detailViewModel)
+        self.navigationController.pushViewController(detailViewController, animated: true)
+    }
+}
