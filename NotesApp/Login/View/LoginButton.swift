95167c65c7e8ef8364b0d22987e6d371f2a3e900
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-10-12 18:29:17 +0300

    added custom login button for login view controller

--- NotesApp/Login/View/LoginButton.swift
+++ NotesApp/Login/View/LoginButton.swift
@@ -1,8 +1,36 @@
-//
-//  LoginButton.swift
-//  NotesApp
-//
-//  Created by Александр Павлицкий on 12.10.2024.
-//
+import UIKit
 
-import Foundation
+final class LoginButton: UIButton {
+    // MARK: - Init
+    init() {
+        super.init(frame: .zero)
+        configureLoginButton()
+    }
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    // MARK: - Life Cycle
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setGradientBackground(frame: self.bounds)
+    }
+    // MARK: - Private Methods
+    func configureLoginButton() {
+        self.setTitle("LOG IN", for: .normal)
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 22, weight: .medium)
+        self.layer.cornerRadius = 6
+        self.layer.masksToBounds = true
+    }
+    func setGradientBackground(frame: CGRect) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = frame
+        gradientLayer.colors = [UIColor.loginButtonFirst.cgColor, UIColor.loginButtonLast.cgColor]
+        gradientLayer.startPoint = CGPoint.init(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint.init(x: 1, y: 0.5)
+        gradientLayer.locations = [0, 1]
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
