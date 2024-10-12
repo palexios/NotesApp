0ea90dc8ffefca696cf23549a56b0834049ddd06
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-10-12 18:32:34 +0300

    added custom login textfield for login viewcontroller

--- NotesApp/Login/View/LoginTextField.swift
+++ NotesApp/Login/View/LoginTextField.swift
@@ -1,8 +1,38 @@
-//
-//  LoginTextField.swift
-//  NotesApp
-//
-//  Created by Александр Павлицкий on 10.10.2024.
-//
+import UIKit
 
-import Foundation
+// MARK: - LoginTextField
+final class LoginTextField: UITextField {
+    // MARK: - Init
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        
+        configureLoginTextField()
+    }
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - Override Methods
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    // MARK: - Private Methods
+    private func configureLoginTextField() {
+        self.textColor = .white
+        self.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        
+        self.attributedPlaceholder = NSAttributedString(string: self.placeholder ?? "", attributes: [
+            .font: UIFont.systemFont(ofSize: 16, weight: .light),
+            .foregroundColor: UIColor.systemGray4
+            ])
+    }
+}
