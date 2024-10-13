e5de4fcfac1e75a5a1c8cc95d1cefb117f21f82e
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-10-13 22:36:00 +0300

    implemented the UITextFieldDelegate to LoginViewController

---
+++ NotesApp/Login/Extensions/LoginViewController+TextFieldDelegate.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+}
